03159624793cb871b33ee409bce20a84c2cae474
unknown <cameron.ung0@gmail.com>
2026-03-06 18:32:43 -0800

    level 4 npc

---
+++ Game Project/Assets/Scripts/Dialogue/LevelDialogue/Level4.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Level4 : DialogueData
+{
+     private void Awake()
+    {
+        DialogueLines = new string[]
+        {
+            "Whiskers! The dash cupcake can allow you to leap forward! Use [Left Shift] to dash forward and get to Evil Tom's lair." 
+        };
+    }
+}
