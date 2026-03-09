9ae959fd6393290dfd3fad8211020076209dec36
Tinh-Phong Nguyen <tinhphong04@gmail.com>
2026-03-09 16:23:51 -0700

    fixed bugs in levels 2 and 3

--- Game Project/Assets/Scripts/Dialogue/LevelDialogue/Level2.2.cs
+++ Game Project/Assets/Scripts/Dialogue/LevelDialogue/Level2.2.cs
@@ -6,7 +6,7 @@ public class Level22 : DialogueData
     {
         DialogueLines = new string[]
         {
-            "*Cat crying noises* Thank the cat lords you found me! I was trying to find some food but the evil Fat Tom locked me in this cage! If you have found the key you can use your Spin to release me!" 
+            "*Cat crying noises* Thank the cat lords you found me! Fat Tom locked me in this cage! If you have found the key you can use your Spin to release me! If you have trouble finding the key, remember that you can move red blocks!" 
         };
     }
 }
